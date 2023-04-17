81ba31ef3cd22e8a6a65e5fe23a3e8fb4ead1936
jordanbreen28 <jordan.breen@perforce.com>
2023-04-17 16:53:09 +0100

    (CONT-788) - Correct RSpec/ImplicitSubject

--- spec/defines/init_spec.rb
+++ spec/defines/init_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe 'mount_iso' do
   let(:facts) { { os: { family: 'windows' } } }
 
   it 'contains exec for mount' do
-    is_expected.to contain_exec("Mount-Iso-#{title}").with(
+    expect(subject).to contain_exec("Mount-Iso-#{title}").with(
       'command' => "Mount-DiskImage -ImagePath '#{title}' -ErrorAction 'Stop'",
       'onlyif' => "if ( (Get-DiskImage -ImagePath '#{title}').Attached ){ exit 1 } else { exit 0 }",
     )
   end
 
   it {
-    is_expected.to contain_exec('Change-Mount-Letter-H').with(
+    expect(subject).to contain_exec('Change-Mount-Letter-H').with(
       'command' => "gwmi win32_volume -Filter \"Label = '\$((Get-DiskImage -ImagePath '#{title}' | Get-Volume).FileSystemLabel)'\" | swmi -Arguments @{DriveLetter=\"H:\"}",
       'onlyif' => "if((Get-DiskImage '#{title}' | Get-Volume).DriveLetter -ieq 'H' ){ exit 1 } exit 0",
     )

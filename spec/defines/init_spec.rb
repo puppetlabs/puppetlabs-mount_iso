1ffa8cf123d3873ec89e30a40f09731d2c3400dc
jordanbreen28 <jordan.breen@perforce.com>
2023-04-17 16:52:38 +0100

    (CONT-788) - Correct RSpec/EmptyLineAfterExample

--- spec/defines/init_spec.rb
+++ spec/defines/init_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe 'mount_iso' do
       'onlyif' => "if ( (Get-DiskImage -ImagePath '#{title}').Attached ){ exit 1 } else { exit 0 }",
     )
   end
+
   it {
     is_expected.to contain_exec('Change-Mount-Letter-H').with(
       'command' => "gwmi win32_volume -Filter \"Label = '\$((Get-DiskImage -ImagePath '#{title}' | Get-Volume).FileSystemLabel)'\" | swmi -Arguments @{DriveLetter=\"H:\"}",
